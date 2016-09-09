9e1d39f432d9fef1a073cd1ac014073e02afcaad
Maryam Syeda <msyed1@live.ca>
2016-09-09 14:22:30 -0400

    First commit.

---
+++ Hello/src/syeda.java
@@ -0,0 +1,15 @@
+
+public class syeda {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		* Displays a "Hello World!" message on the screen
+		*/
+		
+		System.out.println("Hello World!");
+		
+		
+	}
+
+}
