147bbbfcd5bf9428df0f0dab94fe48a91a03e70e
323211557 <323211557@C-4028YR1.tdsb.on.ca>
2016-09-14 14:30:08 -0400

    Beginning stages of Exercise 4 on page 311.

---
+++ NumDigits/src/NumDigits.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class NumDigits {
+
+	/* Create a NumDigit application that includes a recursive method numDigits() that
+	returns the number of digits in its parameter. Number -9 through 9 have one digit; numbers
+	-99 to -10 and 10 to 99 have two digits, and so on. (Hint: the number of digits of a 
+	number 'n' is one more than the number of digits in n/10)
+	*/
+	
+	public static void numdigitsRecursion (int num){
+		
+	}
+	public static void main(String[] args) {
+	Scanner myInput = new Scanner(System.in);
+	numdigitsRecursion(Int.parse(myInput.nextLine()));
+	}
+
+}
