311566f73cac51a3818f8edebfae8c538bccb89f
323211557 <323211557@C-4028YR1.tdsb.on.ca>
2016-09-14 14:28:37 -0400

    An example of a simple recursion method.

---
+++ RecursionExample/src/syeda/RecursionExample.java
@@ -0,0 +1,42 @@
+package syeda;
+
+public class RecursionExample {
+	
+	public static void showRecursion(int num) {
+		System.out.println("Entering method. num = " + num);
+		if (num > 1) {
+			showRecursion(num-1);
+		}
+		System.out.println("Leaving method. num = " + num);
+	}
+
+	public static void main(String[] args) {
+		showRecursion(2);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
