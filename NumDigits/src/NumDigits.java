ec5ada1c1c63bf28b1e80ffe3512183a98ab270f
323211557 <323211557@C-4028YR1.tdsb.on.ca>
2016-09-19 14:30:00 -0400

    Final copy of the Towers of Hanoi Code (Part C)

--- NumDigits/src/NumDigits.java
+++ NumDigits/src/NumDigits.java
@@ -7,13 +7,44 @@ public class NumDigits {
 	-99 to -10 and 10 to 99 have two digits, and so on. (Hint: the number of digits of a 
 	number 'n' is one more than the number of digits in n/10)
 	*/
+	Scanner read = new Scanner(System.in);
 	
 	public static void numdigitsRecursion (int num){
+		Scanner read = new Scanner(System.in);
+		int counter = 0;
+		while (counter > 10) {
+		counter++;	
+		}
 		
+		String number = read.nextLine();
+		System.out.println("Enter. num = " + num);
+		int oneDigit = 1;
+		//race car = race car; dad = dad; mom = mom; 
+		int twoDigits = 2;
+		if (num >= 0 && num <= 99) {
+			if (num <=9){
+				numdigitsRecursion(oneDigit);
+				System.out.println("Leaving method. Number of Digits = " + oneDigit);
+			}
+			else if (num >= 10)
+			numdigitsRecursion(twoDigits);
+			System.out.println("Leaving method. Number of Digits = " + twoDigits);
+		}
+		
+		else if (num < 0 && num >= -99) {
+			if (num >= -9) {
+				numdigitsRecursion(oneDigit);
+				System.out.println("Leaving method. Number of Digits = " + oneDigit);
+			}
+			else if (num <= -10) {
+				numdigitsRecursion(twoDigits);
+				System.out.println("Leaving method. Number of Digits = " + twoDigits);
+			}
+		}
 	}
 	public static void main(String[] args) {
-	Scanner myInput = new Scanner(System.in);
-	numdigitsRecursion(Int.parse(myInput.nextLine()));
+		Scanner read = new Scanner(System.in);
+	
+	numdigitsRecursion(Integer.parseInt(read.nextLine()));
 	}
-
 }
