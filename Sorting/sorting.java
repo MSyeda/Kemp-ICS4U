dac565aadc3fa97e4948d7db9c398781c55858a1
323211557 <323211557@C-4028YR1.tdsb.on.ca>
2016-11-24 14:58:20 -0500

    commit

--- Sorting/sorting.java
+++ Sorting/sorting.java
@@ -32,9 +32,10 @@ public class sorting {
 //			nums[i] = scn.nextInt();
 //		}
 		//mergeSort(nums[size]);
+		mergeSort():
 	}
 
-	public static void mergeSort(int nums[]) {
+	public static void mergeSort(int nums[], int start, int end) {
 		Scanner scn = new Scanner (System.in);
 		int size = scn.nextInt();
 		int nums[] = new int [size];
@@ -51,7 +52,11 @@ public class sorting {
 		if(nums.length == 0){
 			return;
 		}
+		int start = 0;
+		int end = nums.length +1;
+		mid = (start + end)/2;
 		int a = nums.length/2;
+		
 		int leftHalf[] = new int[a];
 		mergeSort(leftHalf);
 		
@@ -67,6 +72,36 @@ public class sorting {
 			for(int j = 0; j < size - i ; j = j + 2){
 				merge();
 			}
+		}
+	}
+	public static void quickSort(int nums[], int start, int end){
+		int pivot = start;
+		int j = nums.length();
+		int i = nums.length();
+		int temp = 0;
+		while(i < nums.length()){
+			while (j < nums.length()){
+				if(nums[i] > nums[j]){
+					quickSort(nums[i]);
+					i++;
+				}
+				else if(nums[i] < nums[j]){
+					temp = nums[i];
+					nums [i] = nums[j];
+					nums[j] = temp;
+					j++;
+				}
+				else if(nums[i] == nums[j]){
+					quickSort(nums[i]);
+					j++;
+					break;
+				}
+				break;
+				quickSort();
+			}
+		}
+		for (int j = 0; j <= nums.length(); j++){
+			pivot = nums[j];
 		}
 	}
 	
